84bab05ac64c250bad24583d99db9ca563ecfba4
Nabin Chand <nbc0117rdx@gmail.com>
2022-11-03 13:04:17 +0545

    get all user form inputs into single state

--- src/hooks/Form.js
+++ src/hooks/Form.js
@@ -2,25 +2,38 @@ import React, { useState } from 'react'
 import '../Css/form.css';
 const Form = () => {
     const [list,setList] = useState([])
-    const [name, setName] = useState('')
-    const [email , setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    // const [name, setName] = useState('')
+    // const [email , setEmail] = useState('')
+    // const [password, setPassword] = useState('')
+    const [person, setPerson] =useState({
+      name:'', email:'', password:''
+    })
+    const handleChange=(e)=>{
+      const name = e.target.name;
+      const value = e.target.value;
+      setPerson({...person,[name]:value})
+    }
      const Update=(e)=>{
         //  list.push(name,email,password)
         // setName([...name,email,password]) 
         e.preventDefault();
-        if(name && email && password){
-        const person ={name,email,password};
-        setList((list)=>{
-          return[...list,person]
-        });
-        setName('')
-        setEmail('') 
-        setPassword('')  
-      }
-      else{
-        console.log("empty value")
-      }
+      //   if(name && email && password){
+      //   const person ={name,email,password};
+      //   setList((list)=>{
+      //     return[...list,person]
+      //   });
+      //   setName('')
+      //   setEmail('') 
+      //   setPassword('')  
+      // }
+      // else{
+      //   console.log("empty value")
+      // }
+          if(person.name && person.email && person.password){
+            const newPerson ={...person}
+            setList([...list, newPerson])
+            setPerson({name:'',email:'',password:''})
+          }
      } 
   return (
     <div className="my_form">
@@ -44,9 +57,9 @@ const Form = () => {
         })}
         </table>
         {/* <form className ="Form_Container"> */}
-          <input type = 'text' placeholder='Enter your name' name ="name" value={name} onChange={(e)=>setName(e.target.value)}/>
-          <input type ='text' placeholder ='Enter your email' name ='email'  value={email} onChange={(e)=>setEmail(e.target.value)}/>
-          <input type="password" placeholder ="Enter your Password" name ="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+          <input type = 'text' placeholder='Enter your name' name ="name" value={person.name} onChange={handleChange}/>
+          <input type ='text' placeholder ='Enter your email' name ='email'  value={person.email} onChange={handleChange}/>
+          <input type="password" placeholder ="Enter your Password" name ="password" value={person.password} onChange={handleChange}/>
           <button className='button_contianer' onClick={Update}>Sign In</button>
           <button className='button_contianer'>Sign Up</button>
         {/* </form> */}
